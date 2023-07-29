2a093dc0408e7d27c1df1634949ee728e272caae
PutEgg <yaoshou@outlook.com>
2023-07-29 16:03:31 +0800

    添加 Forge Mod 基础框架

---
+++ src/main/java/org/totemcraft/pow/PicnicOfTheWorld.java
@@ -0,0 +1,10 @@
+package org.totemcraft.pow;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("pow")
+public class PicnicOfTheWorld {
+    public static final String ModId = "pow";
+
+    public static final String Version = "1.0";
+}
